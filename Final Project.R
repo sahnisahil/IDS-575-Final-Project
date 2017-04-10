f2c0d71412113f70a95ce996635b597097012df8
Sahil Sahni <sahnisahil@users.noreply.github.com>
2017-04-10 01:57:04 -0500

    Update Final Project.R

--- Final Project.R
+++ Final Project.R
@@ -1,26 +1,30 @@
+#Assignment3 - research proposal data pull script
+#Please do not run this script as it pulls from a limited quota of api requests.
+
 install.packages("RGoogleAnalytics")
 library("RGoogleAnalytics")
+
 require(RGoogleAnalytics)
-client.id <- "941727436824-o2pml6tv8djlh7uv2dotevlo58btek3c.apps.googleusercontent.com"
-client.secret <- "NItvBGu--EIeaL9X76-1H7CV"
+client.id <- "941727436824-erco3n81jtiie2q3its107sel9crpkgt.apps.googleusercontent.com"
+client.secret <- "3BQXJPZVI3fTgMcSBFz5YFOK"
 
 #Once created, it will ask if web-cached version is the one you'd like to use, select 2 (No)
 token <- Auth(client.id,client.secret)
 
 # Save the token object for future sessions
 # If this works without requiring additional authentication 
-save(token,file="D:/UIC Study/IDS 575 Business Analytic Statistics")
+save(token,file="C:/Users/pmaitra/Documents/IDS575/token_file")
 
 # Get the Sessions & Transactions for each Source/Medium sorted in 
 # descending order by the Transactions
 
-query.list <- Init(start.date = "2017-03-15",
-                   end.date = "2017-03-16",
+query.list <- Init(start.date = "2016-03-22",
+                   end.date = "2017-03-22",
                    dimensions = "ga:week,ga:deviceCategory,ga:medium",
-                   metrics = "ga:sessions,ga:avgTimeOnPage,ga:bounceRate,ga:pageviewsPerSessions, ga:goalCompletionsAll, ga:avgPageLoadTime, ga:socialInteractions",
+                   metrics = "ga:sessions,ga:searchSessions,ga:avgTimeOnPage,ga:bounceRate,ga:pageviewsPerSession,ga:goal1Completions,ga:avgPageLoadTime", 
                    max.results = 15000,
-                   sort = "-ga:date",
-                   table.id = "ga:9097522")
+                   sort = "-ga:week",
+                   table.id = "ga:10633480")
 ?query.list
 # Create the Query Builder object so that the query parameters are validated
 ga.query <- QueryBuilder(query.list)
@@ -29,14 +33,11 @@ ga.query <- QueryBuilder(query.list)
 ga.data <- GetReportData(ga.query, token)
 
 dimnames(ga.data)
+
 dim(ga.data)
+
 library(dplyr)
 glimpse(ga.data)
 head(ga.data)
 View(ga.data)
-hist(ga.data$hour)
-write.csv(ga.data, "mydata.csv")
-
-
-#Error in ParseDataFeedJSON(GA.Data) : 
-#  code : 403 Reason : User does not have sufficient permissions for this profile.
+hist(ga.data$sessions)
